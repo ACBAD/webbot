fc2d0ad3d51a76ad18104ab0f5fd76f8c5ffca11
ACBAD <53413065+ACBAD@users.noreply.github.com>
2024-05-29 21:10:42 +0800

    lby_cant_attack

--- test.py
+++ test.py
@@ -1,21 +1,44 @@
-import os
-import platform
 import requests
 
 
-def search_img_test():
-    api_key = 'a71bce7cb564ead10b8924be035c34950d97cde2'
-    img_url = 'https://album.biliimg.com/bfs/new_dyn/42ca4aa3d7595e4034bdde12c46143f2236974553.jpg'
-    if not api_key or not img_url:
-        raise NotImplementedError('未配置apikey或图片地址')
-    target_url = f'https://saucenao.com/search.php?db=999&api_key={api_key}&output_type=2&numres=16&url={img_url}'
-    target_url += img_url
-    response = requests.get(target_url)
-    print(response.status_code)
-    with open('output.json', 'w') as file:
-        file.write(response.text)
+def req_generate_img():
+    api_key = 'sk-mMcHBU7KOAJwfQkOufkdpCrPnoK4t8CQRHmYKTZAs6IfImiN'
+    api_key = 'Bearer ' + api_key
+    api_url = 'https://p0.kamiya.dev/api/image/generate'
+    header = {
+        'Authorization': api_key
+    }
+    data = {
+        "type": "text2image",
+        "prompts": "礼花，气球，小巷，看着观众，城市，头发飘起，",
+        "step": 28,
+        "cfg": 12,
+        "seed": 218506577,
+        "sampling": "DPM++ 2M Karras",
+        "width": 768,
+        "height": 512,
+        "model": "original",
+    }
+    response = requests.post(api_url, headers=header, json=data, proxies={})
+    return response.text
+
+
+def check_validation():
+    api_key = 'sk-mMcHBU7KOAJwfQkOufkdpCrPnoK4t8CQRHmYKTZAs6IfImiN'
+    api_key = 'Bearer ' + api_key
+    api_url = 'https://p0.kamiya.dev/api/session/getDetails'
+    header = {
+        'Authorization': api_key
+    }
+    response = requests.get(api_url, headers=header, proxies={})
+    return response.text
 
 
 if __name__ == '__main__':
-    print(platform.system())
-    print(os.path.abspath('.'))
+    output_type = 'console'
+    calling_func = check_validation
+    if output_type == 'console':
+        print(calling_func())
+    elif output_type == 'file':
+        with open('output.json', 'w', encoding='utf-8') as file:
+            file.write(calling_func())
