a8224f78ab9fa347395f8eeb2f6096221828f992
ACBAD <53413065+ACBAD@users.noreply.github.com>
2024-06-03 17:56:54 +0800

    add_developing_mode

--- static/js/redirect_to_hitomi.js
+++ static/js/redirect_to_hitomi.js
@@ -4,20 +4,22 @@ function set_button(action){
     $('#redirect_to_hitomi').prop('disabled', action);
     $('#jm_submit_button').prop('disabled', action);
 }
-
+let msgbox = $('#jm_ajax_response');
 redirect_button.addEventListener('click', function () {
+    msgbox.text('Developing...')
+    return;
     set_button(true);
     if(typeof(EventSource)!=='undefined'){
         const jm_str = $('#jmid').value;
         let source = new EventSource('/bot/redirect_to_hitomi?jm_str=' + jm_str);
-        source.onopen = function (ev){
-            $('#jm_ajax_response').innerText = 'SSE链接已建立';
+        source.onopen = function (){
+            msgbox.text('SSE链接已建立');
         }
         source.onmessage = function (ev){
             /** @type {{echo: string, status: string, type: string}} */
             let server_response = JSON.parse(ev.data);
             if(server_response.type === 'json'){
-                $('#jm_ajax_response').innerText = server_response.echo;
+                msgbox.text(server_response.echo);
                 if(server_response.status === 'error'){
                     set_button(false);
                     source.close();
@@ -28,13 +30,13 @@ redirect_button.addEventListener('click', function () {
             }
         }
         source.onerror = function (ev){
-            $('#jm_ajax_response').innerText = 'SSE链接发生错误';
+            msgbox.text('SSE链接发生错误')
             console.error('SSE ERROR: ', ev);
             source.close();
         }
     }
     else {
-        $('#jm_ajax_response').innerText = '不是哥们，都什么年代了，你的浏览器还不支持SSE？';
+        msgbox.text('不是哥们，都什么年代了，你的浏览器还不支持SSE？');
         set_button(false);
     }
     // $.ajax({
@@ -47,7 +49,7 @@ redirect_button.addEventListener('click', function () {
     //         set_button(false);
     //     },
     //     error: function (){
-    //         $('#jm_ajax_response').innerText = '后端请求出错';
+    //         msgbox.text('后端请求出错');
     //         set_button(false);
     //     }
     // })
